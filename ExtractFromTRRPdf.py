dcb516f87ccff4243ad1188cde64baf2ce89722b
gorri <andreigorincioi2000@gmail.com>
2024-10-25 20:18:13 +0200

    csv

--- ExtractFromTRRPdf.py
+++ ExtractFromTRRPdf.py
@@ -1,24 +1,33 @@
 import pathlib
-from sqlite3 import DatabaseError
+from datetime import datetime as dt
 from PyPDF2 import PdfReader
-from datetime import datetime
-from table_creator import create_workbook_from_tr_converter_data
+ 
+
+cwd = pathlib.Path.cwd()
+FROM_FOLDER = cwd / 'ToConvertTRR'  
+TO_FOLDER = cwd / 'Converted'
+
+if not FROM_FOLDER.is_dir() or not TO_FOLDER.is_dir():
+    raise Exception("Folder \"ToConverted\" and \"Converted\" not found.")
 
 DIVIDER = "§§"
 COLUMNS = ("DATA", "TIPO", "DESCRIZIONE", "IN ENTRATA", "IN USCITA", "SALDO")
 TIPO_TRANSAZIONI = ("Transazione con carta", "Trasferimento", "Pagamento degli interessi")
-
-def main():
-    for path in PATH_FROM.iterdir():
+  
+def main() -> list[list[str]]:
+    for path in FROM_FOLDER.iterdir():
         reader = PdfReader(path)
-        year = get_year(reader.pages[0].extract_text())
-        vals = []
+        first_page_text = reader.pages[0].extract_text()
+        year = get_year(first_page_text)
+        vals = [list(COLUMNS)]
         for page in reader.pages:
             text = page.extract_text()
             data = extract_data(text, year)
-            vals.extend(data)
+            vals.extend(data)                
         sanitize_data(vals)
-        create_workbook_from_tr_converter_data(vals, year)
+
+def save_to_csv(vals:list[list[str]]):
+    pass
 
 def sanitize_data(data:list[list])->list[list]:
     for i in range(len(data)-1):
@@ -31,7 +40,6 @@ def to_float(val:str)-> float:
     val = val.replace(",",".")
     return float(val)
             
-
 def get_year(first_page_text:str) -> str:
     line = first_page_text.split("\n")[1]
     text_finder = "Trade Republic Bank GmbHDATA"
@@ -81,14 +89,3 @@ def extract_from_text(text:str):
         text = text.replace(IN_OUT, "")
         DESCRIPTION = text.strip()
     return [DATA, TIPO, DESCRIPTION, IN_OUT, SALDO]    
-
-if __name__ == '__main__':
-    cwd = pathlib.Path.cwd()
-    PATH_FROM = cwd / 'ToConvert' 
-    PATH_TO = cwd / 'Converted'
-    YEAR = str(datetime.now().year)
-
-    if PATH_FROM.is_dir() and PATH_TO.is_dir():
-        main()
-    else:
-        print("Folder \"ToConverted\" and \"Converted\" not found.")
--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # pyTRConverter
-Converts pdf files from folder ToConvert into Excel files into folder Converted
+Extracts relevant data to csv file. 
+
+## Modules
+**ExtractFromTRRPdf**
+This module contains the logic to extract data from TRR Pdf files from folder ToConvert.
+IMPORTANT: The converted file will have the same name as the origin file.
+1. Looks for pdf files into **ToConvert** folder   
+2. Extracts relevant data from the pdf to cs
+
 
 ### Requirements
 pip install -r requirements.txt 
